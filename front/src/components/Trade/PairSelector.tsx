0c8efb68584916442c3b3421fed0dea3c391349d
lukjcnWq <18819052272@163.com>
2024-10-27 15:41:01 +0800

    FEAT B 1.A DD PAIR

---
+++ front/src/components/Trade/PairSelector.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Select, Space, Typography } from 'antd';
+import { SyncOutlined } from '@ant-design/icons';
+import type { AntdIconProps } from '@ant-design/icons/lib/components/AntdIcon';
+
+const {Option} = Select;
+const {Text} = Typography;
